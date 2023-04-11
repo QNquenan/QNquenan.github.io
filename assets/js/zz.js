40cb59ff79fbe9b49aa809aa29ade3c0df813367
鹊楠吖~ <113289519+QNquenan@users.noreply.github.com>
2023-04-11 20:09:27 +0800

    7.0

--- assets/js/zz.js
+++ assets/js/zz.js
@@ -1,13 +1,4 @@
-		//注释部分是设置1.5秒的定时延迟，timeout结束后加载网页
-		setTimeout(() => {
-			$(".ld").hide();
-			$("body").css("overflow", "auto");
-		}, 1500);
-		//这是根据js判断，页面加载完毕就显示
-		// document.onreadystatechange = function () {
-		// 	if (document.readyState == "complete") {
-		// 			$(".ld").hide();
-		// 			$("body").css("overflow", "auto");
-		// 			$(".ld").attr("style","animation: loadingbg 1.5s linear infinite;");
-		// 	}
-		// }
+window.onload = function () {
+	$(".ld").fadeOut(1000);
+	// $(".loading").hide();
+}
